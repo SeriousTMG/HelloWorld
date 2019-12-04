18076e1a14d53523e479068261ed0f2c6657adf8
max <maxsam2013@mail.ru>
2019-12-04 21:51:33 +0600

    added new button style added post method

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/page2', function (req, res) {
     res.sendFile(__dirname + '/page2.html');
 });
 
+app.post('/', function (req, res) {
+  console.log("POST")
+  res.send('POST request to the homepage');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
